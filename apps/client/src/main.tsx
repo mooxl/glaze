3448d51c801387e29143625fed2016b50bc8f593
Max Schmidt <max.schmidt@outlook.de>
2024-11-02 23:24:36 +0100

    new format config for zed
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
+	routeTree,
+	defaultPreload: "intent",
 });
 
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+	interface Register {
+		router: typeof router;
+	}
 }
 
 const rootElement = document.getElementById("app");
 if (!rootElement) {
-  throw new Error("Root element not found");
+	throw new Error("Root element not found");
 }
 const root = ReactDOM.createRoot(rootElement);
 root.render(<RouterProvider router={router} />);
--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -1,14 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import * as React from "react";
 
 export const Route = createFileRoute("/")({
-  component: HomeComponent,
+	component: HomeComponent,
 });
 
 function HomeComponent() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  );
+	return (
+		<div className="p-2">
+			<h3>Welcome Home!</h3>
+		</div>
+	);
 }
