00cae34fb1209cd8bebd200971a15d5de7fc1b56
Max Schmidt <max.schmidt@outlook.de>
2024-11-02 23:49:50 +0100

    add tailwind
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,21 +1,26 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
-
+import "./index.css";
+import { StrictMode } from "react";
 const router = createRouter({
-	routeTree,
-	defaultPreload: "intent",
+  routeTree,
+  defaultPreload: "intent",
 });
 
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
 
 const rootElement = document.getElementById("app");
 if (!rootElement) {
-	throw new Error("Root element not found");
+  throw new Error("Root element not found");
 }
 const root = ReactDOM.createRoot(rootElement);
-root.render(<RouterProvider router={router} />);
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
