f29e27d6c4bd436c1c68ac304b63be46dad312e1
Max Schmidt <max.schmidt@outlook.de>
2024-11-03 18:14:39 +0100

    setup trpc on the server
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,20 @@
 import { serve } from "@hono/node-server";
+import { trpcServer } from "@hono/trpc-server";
 import { Hono } from "hono";
+import { appRouter } from "./trpc";
 
 const app = new Hono();
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
+
+app.use(
+	"/trpc/*",
+	trpcServer({
+		router: appRouter,
+	}),
+);
 
 const port = 8000;
 console.log(`Server is running on http://localhost:${port}`);
